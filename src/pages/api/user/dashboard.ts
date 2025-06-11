1f772e5e8b3149dea18e8ac3701c2af00768d3fa
ghufron akbar <alan.040219.gam@gmail.com>
2025-06-12 04:13:11 +0700

    Add Overview User

--- src/pages/api/user/dashboard.ts
+++ src/pages/api/user/dashboard.ts
@@ -1,8 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import apiAuth from "@/middleware/api-auth";
+import db from "@/config/db";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({});
+  const userId = req.decoded?.id;
+  const orders = await db.order.findMany({
+    where: {
+      userId,
+    },
+    select: {
+      status: true,
+    },
+  });
+  const data = {
+    pendingOrders: orders.filter((order) => order.status === "Pending").length,
+    completedOrders: orders.filter((order) => order.status === "Selesai")
+      .length,
+    waitingOrders: orders.filter((order) => order.status === "Dibayar").length,
+  };
+  return res.status(200).json({ message: "Success", data });
 }
 
 export default apiAuth(handler, ["User"]);
